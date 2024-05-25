72aa8ccc45c203d17ba323c1a5a056054497febb
madhanmohans <sherlockholmes.missme@gmail.com>
2024-05-25 15:48:52 +0530

    added request params for get users

--- src/main/java/com/example/spring/mobile_app_ws/ui/controller/UserController.java
+++ src/main/java/com/example/spring/mobile_app_ws/ui/controller/UserController.java
@@ -6,12 +6,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users") // http://localhost/8080/users
 public class UserController {
 
+    @GetMapping
+    public String getUsers(@RequestParam(value = "page") int page, 
+                            @RequestParam(value = "limit") int limit) {
+        return "get users was called with page " + page + " and limit " + limit;
+    }
+    
     @GetMapping(path="/{userID}")
     public String getUser(@PathVariable("userID") String userID) {
         return "Get user was called with userID " + userID;
