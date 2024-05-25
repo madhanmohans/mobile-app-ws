eddc9b480e78824b2c9c128f1482e87ead041d40
madhanmohans <sherlockholmes.missme@gmail.com>
2024-05-25 17:01:31 +0530

    implemented create usert

--- src/main/java/com/example/spring/mobile_app_ws/ui/controller/UserController.java
+++ src/main/java/com/example/spring/mobile_app_ws/ui/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.spring.mobile_app_ws.ui.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -8,10 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.spring.mobile_app_ws.ui.model.request.UserDetailsRequest;
 import com.example.spring.mobile_app_ws.ui.model.response.UserRest;
 
 @RestController
@@ -36,12 +39,22 @@ public class UserController {
         returnValue.setEmail("example@email.com");
         returnValue.setFirstName("Alex");
         returnValue.setLastName("Hormozzi");
-        return new ResponseEntity<UserRest>(HttpStatus.BAD_REQUEST); // JSON default return type
+        return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK); // JSON default return type
     }
     
-    @PostMapping
-    public String createUser() {
-        return "Create user was called";
+    @PostMapping(consumes = {
+        MediaType.APPLICATION_XML_VALUE,
+        MediaType.APPLICATION_JSON_VALUE
+        }, produces = {
+        MediaType.APPLICATION_XML_VALUE,
+        MediaType.APPLICATION_JSON_VALUE
+        })
+    public ResponseEntity<UserRest> createUser(@RequestBody UserDetailsRequest createUserRequest) {
+        UserRest returnValue = new UserRest();
+        returnValue.setEmail(createUserRequest.getEmail());
+        returnValue.setFirstName(createUserRequest.getFirstName());
+        returnValue.setLastName(createUserRequest.getLastName());
+        return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
     }    
     
     @PutMapping
